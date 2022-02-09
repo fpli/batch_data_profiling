548d30aba521604c3e89004db619291dcbada200
fangpli <fangpli@ebay.com>
2022-02-09 14:36:57 +0800

    I add config info to run different data analysis

--- profilingjob/src/main/scala/com/ebay/dataquality/controller/DataProfilingController.scala
+++ profilingjob/src/main/scala/com/ebay/dataquality/controller/DataProfilingController.scala
@@ -9,13 +9,13 @@ import com.ebay.dataquality.util.EnvUtil
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-class DataProfilingController extends TController with Loggable{
+class DataProfilingController extends TController with Loggable {
 
   private val dataProfilingService = new DataProfilingService
 
   override def dispatch(option: Option[Parameters]): Unit = {
     val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-    var yesterday = LocalDate.now().minusDays(1).format(dateTimeFormatter)
+    var yesterday = LocalDate.now().minusDays(2).format(dateTimeFormatter)
     var env = "prod"
     try {
       if (option.nonEmpty){
